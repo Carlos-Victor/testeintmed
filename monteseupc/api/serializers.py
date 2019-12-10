1f027ce1b2d86de02fc34eae26e3cf214636188d
Carlos Victor <carlosvictortecprof@gmail.com>
2019-12-10 19:42:29 -0300

    Adicionado validacao de slots de memoria

--- monteseupc/api/serializers.py
+++ monteseupc/api/serializers.py
@@ -10,11 +10,14 @@ def validator_marca_procesador(value):
     return value
 
 def validacao_placa_de_video(value):
-    print(value['placa_de_video'])
     if value['placa_mae'].video_integrado == False and value['placa_de_video'] == None:
         raise ValidationError("A placa mãe não possui video integrado, escolha uma placa de vídeo")
     return value
-        
+
+def validacao_quantidade_de_slots(value):
+    if value['qnt_memoria'] > value['placa_mae'].slots:
+        raise ValidationError('A Placa mãe so possui {} slots, selecione uma quantidade menor ou igual.'.format(value['placa_mae'].slots))
+    return value
     
 
 
@@ -51,5 +54,6 @@ class MonteseupcSerializer(ModelSerializer):
                   'qnt_memoria', 'tamanho_da_memoria', 'placa_de_video']
         validators = [
             validator_marca_procesador,
-            validacao_placa_de_video
+            validacao_placa_de_video,
+            validacao_quantidade_de_slots
         ]
